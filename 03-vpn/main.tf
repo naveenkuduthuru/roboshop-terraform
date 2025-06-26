d75b97d1381b22187ab3850c77e23b4d322482ab
joindevops07 <naveen@devops.com>
2025-06-26 13:06:26 +0530

    terrafrom

--- 03-vpn/main.tf
+++ 03-vpn/main.tf
@@ -1,6 +1,6 @@
 module "vpn" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
-  ami = data.aws_ami.centos8.id
+  ami = data.aws_ami.ec2.id
   name                   = "${local.ec2_name}-vpn"
   instance_type          = "t3.small"
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
